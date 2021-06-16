685cfacae0e99814554460b2a81d5c6d6f4a839e
Kaijun Jiang <kaijun.jiang@hcl.com>
2021-06-16 11:29:12 +0800

    workaround

---
+++ volt-mx-tutorials/_next/static/webpack/styles.22d1a189525ce6ce1d55.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./src/components/HikeBreadCrumb/style.scss":
+false,
+
+/***/ "./src/components/KonyButton/style.scss":
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false
+
+})
---
+++ volt-mx-tutorials/_next/static/webpack/styles.447d04b3baf0166bb185.hot-update.js
@@ -0,0 +1,57 @@
+webpackHotUpdate_N_E("styles",{
+
+/***/ "./src/components/HikeBreadCrumb/style.scss":
+false,
+
+/***/ "./src/components/KonyButton/style.scss":
+false,
+
+/***/ "./src/components/KonyGravatar/style.scss":
+false,
+
+/***/ "./src/components/KonyLayout/style.scss":
+false,
+
+/***/ "./src/components/KonySearch/style.scss":
+false,
+
+/***/ "./src/components/MarketplaceHeader/style.scss":
+false,
+
+/***/ "./src/components/PageNotFound/style.scss":
+false,
+
+/***/ "./src/components/SiteFooter/style.scss":
+false,
+
+/***/ "./src/components/SiteHeader/style.scss":
+false,
+
+/***/ 13:
+false,
+
+/***/ 14:
+false,
+
+/***/ 15:
+false,
+
+/***/ 16:
+false,
+
+/***/ 17:
+false,
+
+/***/ 18:
+false,
+
+/***/ 19:
+false,
+
+/***/ 6:
+false,
+
+/***/ 7:
+false
+
+})
